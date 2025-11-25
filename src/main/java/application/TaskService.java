77a171bdebb2cf49fce253c32159ca54aa91ad67
Andrés <andyrouge81@gmail.com>
2025-11-25 13:28:51 +0100

    Feature/task service (#20)
    * feat creating TaskService
    
    * feat: namings and structure
    
    * feat:add .gitignore
    
    * Delete src/.gitignore
    
    * Delete .idea directory
    
    * Delete .DS_Store
    
    * Fix typos in TaskService.java
    
    ---------
    
    Co-authored-by: Pau Gaston Boza <paugaston@hotmail.com>

---
+++ src/main/java/application/TaskService.java
@@ -0,0 +1,104 @@
+package application;
+
+import task.repository.TaskRepository;
+
+import java.sql.SQLDataException;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository){
+        this.taskRepository = taskRepository;
+
+    }
+
+    //CRUD
+
+    // 1.Create
+    public void addTask(String title, String text, LocalDate.parse(expireDate), PriorityType priority){
+
+        if(text == null || text.isBlank()){
+            throw new IllegalArgumentException("You have to enter a task.");
+        }
+
+        if(priority == null){
+            priority = PriorityType.MIDDLE;
+
+        }
+
+        Task task = new Task();
+        task.setTitle(title);
+        task.setContent(text);
+        task.setCreationDate(LocalDateTime.now());
+        task.setExpirationDate(expireDate);
+        task.setPriority(priority);
+        task.setDoneStatus(DoneType doneStatus);
+
+        taskRepository.addTask(task);
+        System.out.println("Task created with success");
+
+
+    }
+
+    //Read ALL
+    public List<Task> getAllTasks() throws SQLDataException {
+        return taskRepository.getAllTasks();
+    }
+
+
+    // Read ONE
+    public Task getTaskById(int id){
+        return taskRepository.findById(id);
+    }
+
+    //Update
+
+    public void markTaskCompleted(int id) throws SQLException {
+        Task task = taskRepository.getTask(id);
+
+        if (task == null){
+            throw new IllegalArgumentException("The task with id "+id+" does not exists");
+        }
+
+        task.setDoneStatus(DoneStatus.COMPLETE);
+        taskRepository.updateTask(task);
+        System.out.println("Task completed");
+
+
+    }
+
+    public void updateTask(int id, String text, String title, LocalDateTime expirationDate, PriorityType priority) throws SQLException{
+        Task task = taskRepository.getTask(id);
+        if(task == null){
+            throw new IllegalArgumentException("The task with id "+id+" does not exists");
+        }
+        if(text != null && !text.isBlank()){
+            task.setContent(text);
+
+        }
+        if (title != null && title.isBlank()){
+            task.setTitle(title);
+        }
+
+        if(expirationDate != null){
+            task.setExpirationDate(expirationDate);
+        }
+        if(priority != null){
+            task.setPriority(priority);
+        }
+
+        taskRepository.updateTask(task);
+    }
+
+    //Delete
+
+    public void deleteTask(int id)throws SQLException{
+        taskRepository.delete(id);
+        System.out.println("Task Deleted");
+    }
+
+}
