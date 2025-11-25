00890ed0a91e5825bf23f6bfaae1749afe90c399
PauG <paugaston@hotmail.com>
2025-11-25 21:06:57 +0100

    refactoring TaskRepository to extend EntityRepository

--- src/main/java/task/repository/TaskRepository.java
+++ src/main/java/task/repository/TaskRepository.java
@@ -1,57 +1,13 @@
 package task.repository;
 
-import common.exception.DataAccessException;
-import infrastructure.sql.dao.GenericDAO;
+import common.dao.GenericDAO;
+import common.repository.EntityRepository;
 import task.model.Task;
-import java.sql.SQLException;
-import java.util.List;
 
-public class TaskRepository {
+public class TaskRepository extends EntityRepository<Task> {
 
-    private final GenericDAO<Task> taskDAO;
-
-    public TaskRepository(GenericDAO<Task> taskDAO) {
-        this.taskDAO = taskDAO;
-    }
-
-    public void addTask(Task task) {
-        try {
-            taskDAO.insert(task);
-        } catch (SQLException e) {
-            throw new DataAccessException("Error inserting task", e);
-        }
-    }
-
-    public Task getTask(int id) {
-        try {
-            return taskDAO.findById(id);
-        } catch (SQLException e) {
-            throw new DataAccessException("Error finding task with id " + id, e);
-        }
-    }
-
-    public List<Task> getAllTasks() {
-        try {
-            return taskDAO.findAll();
-        } catch (SQLException e) {
-            throw new DataAccessException("Error retrieving all tasks", e);
-        }
-    }
-
-    public void updateTask(Task task) {
-        try {
-            taskDAO.update(task);
-        } catch (SQLException e) {
-            throw new DataAccessException("Error updating task with id " + task.getId(), e);
-        }
-    }
-
-    public void removeTask(int id) {
-        try {
-            taskDAO.delete(id);
-        } catch (SQLException e) {
-            throw new DataAccessException("Error deleting task with id " + id, e);
-        }
+    public TaskRepository(GenericDAO<Task> taskDao) {
+        super(taskDao);
     }
 
 }
