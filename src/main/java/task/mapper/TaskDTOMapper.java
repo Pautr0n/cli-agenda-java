a2bf2e41df63f7c4e062558315aa6cad1d15ff36
Pau Gaston Boza <paugaston@hotmail.com>
2025-12-01 11:24:25 +0100

    Refactor/task service (#45)
    * applying IDE warnings
    
    * refactoring validate methods (strings, and parenthesis)
    
    * refactoring deleteTask(), TaskDTO already checks if id does not exists.
    
    * refactoring updteTask(), implementing the business logic to change task variables, and deleting database id existence verification
    
    * implementing a new method getTasksByStatus to substitute getPendingTasks and getCompletedTasks()
    
    * commenting createTask for a possible refactor based on new changes to be applied to Repository and DAO insert() methods
    
    * refactoring calls to TaskDTOMapper methods.
    
    * refactoring createTask() to implement the Task object returned by insert();
    
    * refactoring getTaskByStatus
    
    * refactoring and checking with tests all changes are working well

--- src/main/java/task/mapper/TaskDTOMapper.java
+++ src/main/java/task/mapper/TaskDTOMapper.java
@@ -33,6 +33,26 @@ public class TaskDTOMapper {
         return task;
     }
 
+    public static Task dtoToTask(TaskUpdateDTO dto){
+        Task task = new Task();
+
+        task.setTitle(dto.title());
+        task.setContent(dto.content());
+        task.setCreationDate(LocalDateTime.now());
+        task.setExpirationDate(LocalDate.parse(dto.expirationDate()));
+
+        PriorityType priority = PriorityType.MEDIUM;
+        if(dto.priority() != null && !dto.priority().isBlank()){
+            priority = PriorityType.valueOf(dto.priority().toUpperCase());
+        }
+
+        task.setPriority(priority);
+
+        task.setDoneStatus(DoneType.NOTDONE);
+
+        return task;
+    }
+
     public static TaskOutputDTO taskToDTO(Task task){
 
         String priority = task.getPriority() != null
