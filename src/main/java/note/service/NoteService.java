66d2eda96bb2ff73563e256a76a64831250985c6
JordiCasas87 <Jordicasas87@gmail.com>
2025-12-02 18:46:04 +0100

    feat: implementacion nuevos cambios en NoteService

--- src/main/java/note/service/NoteService.java
+++ src/main/java/note/service/NoteService.java
@@ -1,19 +1,14 @@
 package note.service;
 
-
 import common.exception.DataAccessException;
+import common.exception.EntityNotFoundException;
+import common.exception.ValidationException;
 import note.model.Note;
 import note.repository.NoteRepository;
-import task.dto.TaskIdDTO;
-import task.dto.TaskOutputDTO;
-import task.dto.TaskUpdateDTO;
-import task.enums.PriorityType;
-import task.mapper.TaskDTOMapper;
-import task.model.Task;
-import java.time.LocalDate;
-import java.util.List;
+import task.dto.TaskDTO;
 
 public class NoteService {
+
 
     private final NoteRepository noteRepository;
 
@@ -22,185 +17,163 @@ public class NoteService {
 
     }
 
-
-    //CRUD a preparar Variables de la nota
-    //
-    //Titulo
-    //Texto
-    //Fecha de creación
-
-//CRUD
-
-    // 1.Create
-    public NoteOutputDTO createNote (NoteDTO dto) {
+    //CRUD
+    // Create NOTE
+    public NoteOutputDTO createNote(NoteDTO dto) {
 
         try {
             validateNoteDTOCreate(dto);
 
             Note note = NoteDTOMapper.dtoToNote(dto);
-            note = taskRepository.add(note);
+            note = noteRepository.add(note);
 
             return NoteDTOMapper.noteToDTO(note);
 
+        } catch (DataAccessException e) {
+            throw e;
         } catch (Exception e) {
-            throw new DataAccessException("Error creating note", e);
+            throw new ServiceException("NoteService [createNtoe]: Unexpected error creating note", e);
         }
+
     }
 
-    // continuar a partir de aqui
+    //Read One NOTE
 
-    //Read One
-    public TaskOutputDTO getTaskById(TaskIdDTO id) {
+    public NoteOutputDTO getNoteById(NoteIdDTO id) {
         try {
-            validateTaskId(id);
-            Task task = taskRepository.getById(id.id());
+            validateNoteId(id);
+            Note note = noteRepository.getById(id.id());
 
-            if (task == null) {
-                throw new IllegalArgumentException("Task with id " + id.id() + " not found");
+            if (note == null) {
+                throw new IllegalArgumentException("Note with id " + id.id() + " not found");
             }
 
-            return TaskDTOMapper.taskToDTO(task);//PAU: modificado el Mapper:
+            return NoteDTOMapper.noteToDTO(note);
 
+        } catch (EntityNotFoundException | DataAccessException | ValidationException e) {
+            throw e;
         } catch (Exception e) {
-            throw new DataAccessException("Error retrieving task with id: " + id.id(), e);
+            throw new ServiceException("NoteService [getNoteById]: Unexpected error retrieving note with id " + id.id(), e);
         }
+
     }
 
-    //Read ALL
-    public List<TaskOutputDTO> getAllTasks() {
+    //Read ALL NOTES
+    public List<NoteOutputDTO> getAllNotes() {
         try {
 
-            return taskRepository.getAll().stream()
-                    .map(TaskDTOMapper::taskToDTO)  //PAU: modificado el Mapper:
+            return noteRepository.getAll().stream()
                     .toList();
 
+        } catch (DataAccessException e) {
+            throw e;
         } catch (Exception e) {
-            throw new DataAccessException("Error retrieving task from database.", e);
+            throw new ServiceException("NoteService [getAllNotes]: Unexpected error retrieving note", e);
         }
-    }
+    }g
 
 
-    //Update
-    public TaskOutputDTO updateTask(TaskUpdateDTO dto) {
+    //Update NOTE
+    public NoteOutputDTO updateNote(NoteUpdateDTO dto) {
+        validateNoteUpdate(dto);
+
         try {
+            Note note = noteRepository.getById(dto.id());
 
-            validateTaskUpdate(dto);
-
-            Task task = taskRepository.getById(dto.id());
+            if (note == null) {
+                throw new EntityNotFoundException("Note with id " + dto.id() + " not found");
+            }
 
             if (dto.title() != null && !dto.title().isBlank()) {
-                task.setTitle(dto.title());
+                note.setTitle(dto.title());
             }
 
             if (dto.content() != null && !dto.content().isBlank()) {
-                task.setContent(dto.content());
+                note.setContent(dto.content());
             }
 
-            if (dto.expirationDate() != null && !dto.expirationDate().isBlank()) {
-                task.setExpirationDate(LocalDate.parse(dto.expirationDate()));
-            }
 
-            if (dto.priority() != null && !dto.priority().isBlank()) {
-                task.setPriority(PriorityType.valueOf(dto.priority().toUpperCase()));
-            }
+            noteRepository.update(note);
 
-            taskRepository.update(task);
+            return NoteDTOMapper.noteToDTO(note);
 
-            return TaskDTOMapper.taskToDTO(task);
-
+        } catch (EntityNotFoundException | DataAccessException e) {
+            throw e;
         } catch (Exception e) {
-            throw new DataAccessException("Error updating task with id: " + dto.id() + " ", e);
+            throw new ServiceException("NoteService [updateNote]: Unexpected error updating note id=" + dto.id(), e);
         }
+
     }
 
-    //Delete task
-    public void deleteTask(TaskIdDTO id) {
+
+    //Delete NOTE
+    public void deleteNote(NoteIdDTO id) {
 
         try {
-
-            validateTaskId(id);
-
-            taskRepository.remove(id.id());
-
-
-        } catch (DataAccessException e) {
-            throw new DataAccessException("Exception while deleting task with id: " + id.id(), e);
+            validateNoteId(id);
+            noteRepository.remove(id.id());
+        } catch (EntityNotFoundException | DataAccessException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new ServiceException("NoteService [deleteTask]: Unexpected error deleting note id=" + id.id(), e);
         }
     }
 
 
 
     //******************************************************************
-/*
+
+
     //Validations
-    private void validateTaskDTOCreate(TaskDTO dto) {
+    private void validateNoteDTOCreate(TaskDTO dto) {
 
         if (dto == null) {
-            throw new IllegalArgumentException("DTO record instance cannot be null");
+            throw new  ValidationException("DTO record instance cannot be null");
         }
 
         if (dto.content() == null || dto.content().isBlank()) {
-            throw new IllegalArgumentException("Content cannot be empty");
+            throw new ValidationException("Content cannot be empty");
         }
 
         if (dto.title() == null || dto.title().isBlank()) {
-            throw new IllegalArgumentException("Title cannot be empty");
+            throw new ValidationException("Title cannot be empty");
         }
 
-        if (dto.expirationDate() == null || dto.expirationDate().isBlank()) {
-            throw new IllegalArgumentException("Expiration date cannot be empty");
-        }
-
-        try {
-            LocalDate.parse(dto.expirationDate());
-        } catch (Exception e) {
-            throw new IllegalArgumentException("Expiration date format must be yyyy-MM-dd", e);
-        }
 
     }
 
-    private void validateTaskId(TaskIdDTO id) {
+    private void validateNoteId(NoteIdDTO id) {
         if (id == null) {
-            throw new IllegalArgumentException("DTO record instance cannot be null");
+            throw new ValidationException("DTO record instance cannot be null");
         }
         if (id.id() == null) {
-            throw new IllegalArgumentException("Task id value cannot be null");
+            throw new ValidationException("Note id value cannot be null");
         }
         if (id.id() <= 0) {
-            throw new IllegalArgumentException("Invalid id: " + id.id());
+            throw new ValidationException("Invalid id: " + id.id());
         }
     }
 
 
-    private void validateTaskUpdate(TaskUpdateDTO dto) {
+    private void validateNoteUpdate(NoteUpdateDTO dto) {
 
         if (dto == null) {
-            throw new IllegalArgumentException("DTO record instance cannot be null");
+            throw new ValidationException("DTO record instance cannot be null");
         }
 
         if (dto.id() <= 0) {
-            throw new IllegalArgumentException("Invalid id.");
+            throw new ValidationException("Invalid id.");
         }
 
         if ((dto.title() == null || dto.title().isBlank()) &&
-                (dto.content() == null || dto.content().isBlank()) &&
-                (dto.expirationDate() == null || dto.expirationDate().isBlank()) &&
-                (dto.priority() == null || dto.priority().isBlank())) {
+                (dto.content() == null || dto.content().isBlank())) {
 
-
-            throw new IllegalArgumentException("No fields provided to update");
-
-        }
-
-        if (dto.expirationDate() != null && !dto.expirationDate().isBlank()) {
-            try {
-                LocalDate.parse(dto.expirationDate());
-            } catch (Exception e) {
-                throw new IllegalArgumentException("Expiration date format must be yyyy-MM-dd");
-            }
+            throw new ValidationException("No fields provided to update");
         }
 
     }
-*/
+
+
 }
+
 
