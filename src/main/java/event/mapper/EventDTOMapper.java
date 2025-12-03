256b58e4d5d280dc9682353bba16cc09c5450c3f
Jordi Casas <jordicasas87@gmail.com>
2025-12-03 13:23:57 +0100

    feat:  implementacion mapper de event (#78)

--- src/main/java/event/mapper/EventDTOMapper.java
+++ src/main/java/event/mapper/EventDTOMapper.java
@@ -1,4 +1,57 @@
 package event.mapper;
 
+import event.dto.EventDTO;
+import event.dto.EventOutputDTO;
+import event.dto.EventUpdateDTO;
+import event.model.*;
+import java.time.LocalDateTime;
+
 public class EventDTOMapper {
+
+    public static Event dtoToEvent(EventDTO dto) {
+        if (dto == null) {
+            throw new IllegalArgumentException("EventDTO cannot be null");
+        }
+
+        Event event = new Event();
+        event.setTitle(dto.title());
+        event.setContent(dto.content());
+        event.setCreationDate(dto.creationDate() != null ? dto.creationDate() : LocalDateTime.now());
+        event.setExpirationDate(dto.expirationDate());
+        return event;
+    }
+
+    public static Event dtoToEvent(EventUpdateDTO dto) {
+        if (dto == null) {
+            throw new IllegalArgumentException("EventUpdateDTO cannot be null");
+        }
+
+        Event event = new Event();
+        event.setId(dto.id());
+
+
+        if (dto.title() != null && !dto.title().isBlank()) {
+            event.setTitle(dto.title());
+        }
+
+        if (dto.content() != null && !dto.content().isBlank()) {
+            event.setContent(dto.content());
+        }
+
+        return event;
+    }
+
+    public static EventOutputDTO eventToDTO(Event event) {
+        if (event == null) {
+            throw new IllegalArgumentException("Event cannot be null");
+        }
+
+        return new EventOutputDTO(
+                event.getId(),
+                event.getTitle(),
+                event.getContent(),
+                event.getCreationDate(),
+                event.getExpirationDate()
+        );
+    }
 }
